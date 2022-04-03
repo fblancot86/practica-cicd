ee8ecae142fb07cf58674038c08ab91a8a97a450
Francesc Blanco <cloifar86@gmail.com>
2022-04-03 18:20:22 +0200

    check-s3.sh

--- check-s3.sh
+++ check-s3.sh
@@ -7,19 +7,14 @@ in="${1:-s3.list}"
 while IFS= read -r bucket
 do
 	echo "Working on $bucket ..."
-    SIZE="$(aws s3 ls s3://$bucket --recursive --summarize | grep Size | awk '{print $3}' 2> /dev/null)"
+    SIZE="$(aws s3 ls s3://$bucket --recursive --summarize 2> /dev/null | grep Size | awk '{print $3}')"
 
-    echo "$bucket $SIZE"
-
-    if [$SIZE != ""]
+    if [ $SIZE -gt 20971520 ]
     then
-        if [$SIZE -gt 20971520]
-        then
-            echo "Emptying $bucket ..."
-            aws s3 rm s3://$bucket --recursive
-        else
-            echo "Bucket size under 20 MiB ($SIZE Bytes) ..."
-        fi
+        echo "Emptying $bucket ..."
+        aws s3 rm s3://$bucket --recursive
+    else
+        echo "Bucket size under 20 MiB ($SIZE Bytes) ..."
     fi
 done < "${in}"
 
